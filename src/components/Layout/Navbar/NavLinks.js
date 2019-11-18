ad6924ed913b6f7e400d3e0654bf99b0886e094b
Richard Booth <admin@rickbooth.me>
2019-11-18 18:52:53 +0000

    Fix window undefined errors

--- src/components/Layout/Navbar/NavLinks.js
+++ src/components/Layout/Navbar/NavLinks.js
@@ -1,18 +1,14 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import Link from 'gatsby-link'
 import { Link as SLink } from 'react-scroll'
 
 const NavLinks = ({ NavItem }) => {
-  const [path, setPath] = useState(null)
   const Links = ['home', 'about me', 'contact']
-  useEffect(() => {
-    if (window !== 'undefined') setPath(window.location.pathname)
-  }, [window.location.pathname])
   return (
     <>
       {Links.map((link, index) => (
         <NavItem key={index}>
-          {path === '/' ? (
+          {typeof window !== 'undefined' && window.location.pathname === '/' ? (
             <SLink smooth hashSpy={true} to={link.replace(' ', '-')}>
               {link}
             </SLink>
